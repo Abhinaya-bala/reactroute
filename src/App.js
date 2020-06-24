ec74183a7ff6a47bc15984a7eac0b2d27b346e01
Abhinaya-bala <abhinayafeb19@gmail.com>
2020-06-24 10:33:02 +0530

    Creted route

--- src/App.js
+++ src/App.js
@@ -1,24 +1,15 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import { Route, Switch } from 'react-router-dom'
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div >
+      <Route exact path="/" render={() => <Home />} />
+      <Route path="/cart" render={() => <Cart />} />
+      <Route path="/login" render={() => <Login />} />
+      <Route path="/order" render={() => <Order />} />
+      <Route path="/product" render={() => <Product />} />
     </div>
   );
 }
